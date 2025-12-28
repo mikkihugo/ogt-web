2871ca2c3bd80a7925cca2d68b745fe2637007ff
mikkihugo <mikkihugo@users.noreply.github.com>
2025-12-28 18:25:28 +0000

    build: revert pkgs-x86 error, use pkgs.lib.fakeHash

--- flake.nix
+++ flake.nix
@@ -155,7 +155,7 @@
           src = ./.;
           
           # NPM configuration - hash will be recalculated
-          npmDepsHash = pkgs-x86.lib.fakeHash;
+          npmDepsHash = pkgs.lib.fakeHash;
           makeCacheWritable = true;
           npmFlags = [ "--legacy-peer-deps" ];
           dontNpmBuild = true;  # We handle build in buildPhase
