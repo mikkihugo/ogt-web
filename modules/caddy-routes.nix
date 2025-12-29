bedd18a5d4f65b30cf4b1668d05fc9c287936f6e
mikkihugo <mikkihugo@users.noreply.github.com>
2025-12-29 03:46:57 +0000

    fix(ops): serve medusa admin statically to resolve 502 crash

--- modules/caddy-routes.nix
+++ modules/caddy-routes.nix
@@ -1,57 +1,77 @@
 # Caddy Routes module - Centralized reverse proxy configuration
-{ config, lib, ... }:
+{ config, lib, self, ... }:
 
 with lib;
 
-{
-  options.ogt.caddy = {
-    enable = mkEnableOption "OGT Caddy reverse proxy routes";
-    
-    domain = mkOption {
-      type = types.str;
-      default = "orgasmtoy.com";
-      description = "Primary domain for the platform";
-    };
-  };
 
-  config = mkIf config.ogt.caddy.enable {
-    services.caddy = {
-      enable = true;
+  let
+    medusaAdmin = self.packages.aarch64-linux.medusa-admin;
+  in
+
+  {
+    options.ogt.caddy = {
+      enable = mkEnableOption "OGT Caddy reverse proxy routes";
       
-      virtualHosts = {
-        # Main storefront (orgasmtoy.com)
-        "orgasmtoy.com" = {
-          extraConfig = ''
-            reverse_proxy localhost:3000
-          '';
-        };
-        
-        "www.orgasmtoy.com" = {
-          extraConfig = ''
-            redir https://orgasmtoy.com{uri} permanent
-          '';
-        };
+      domain = mkOption {
+        type = types.str;
+        default = "orgasmtoy.com";
+        description = "Primary domain for the platform";
+      };
+    };
 
-        # Blog Redirect (ownorgasm.com -> orgasmtoy.com/blog)
-        "ownorgasm.com" = {
-          extraConfig = ''
-            redir https://orgasmtoy.com/blog permanent
-          '';
-        };
+    config = mkIf config.ogt.caddy.enable {
+      services.caddy = {
+        enable = true;
         
-        "www.ownorgasm.com" = {
-          extraConfig = ''
-            redir https://orgasmtoy.com/blog permanent
-          '';
-        };
-        
-        # Medusa Admin/API
-        "admin.orgasmtoy.com" = {
-          extraConfig = ''
-            reverse_proxy localhost:9000
-          '';
-        };
+        virtualHosts = {
+          # Main storefront (orgasmtoy.com)
+          "orgasmtoy.com" = {
+            extraConfig = ''
+              reverse_proxy localhost:3000
+            '';
+          };
+          
+          "www.orgasmtoy.com" = {
+            extraConfig = ''
+              redir https://orgasmtoy.com{uri} permanent
+            '';
+          };
+
+          # Blog Redirect (ownorgasm.com -> orgasmtoy.com/blog)
+          "ownorgasm.com" = {
+            extraConfig = ''
+              redir https://orgasmtoy.com/blog permanent
+            '';
+          };
+          
+          "www.ownorgasm.com" = {
+            extraConfig = ''
+              redir https://orgasmtoy.com/blog permanent
+            '';
+          };
+          
+          # Medusa Admin/API
+          "admin.orgasmtoy.com" = {
+            extraConfig = ''
+              root * ${medusaAdmin}
+
+              handle_path /app/* {
+                file_server
+                try_files {path} {path}/ /index.html
+              }
+
+              handle /app {
+                redir /app/ permanent
+              }
+
+              handle {
+                reverse_proxy localhost:9000
+              }
+            '';
+          };
+      };
     };
   };
-  };
+
+
 }
